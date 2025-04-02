0a0ec07dab995e8a184c47d65034573f4060827b
equinox766 <Equinox766@gmail.com>
2025-04-02 16:57:22 -0300

    first commit

---
+++ src/main/java/org/equinox/paymentchain/PaymentchainApplication.java
@@ -0,0 +1,13 @@
+package org.equinox.paymentchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentchainApplication.class, args);
+    }
+
+}
